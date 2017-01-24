bad5ffa8398a5be402710a3190e223b054334e87
zhangzibin <619146134@qq.com>
2017-01-24 17:08:36 +0800

    资源库设计模式

--- index.php
+++ index.php
@@ -6,38 +6,10 @@ function my_autoload($className)
 }
 spl_autoload_register("my_autoload");
 
-/*use League\Pipeline\Pipeline;
-use League\Pipeline\StageInterface;
-
-class TimesTwoStage implements StageInterface
-{
-    public function __invoke($payload)
-    {
-        // TODO: Implement __invoke() method.
-        return $payload * 2;
-    }
-}
-
-class AddOneStage implements StageInterface
-{
-    public function __invoke($payload)
-    {
-        // TODO: Implement __invoke() method.
-        return $payload + 1;
-    }
-}
-
-$pipeline = (new Pipeline())
-    //->pipe(new TimesTwoStage())
-    ->pipe(new AddOneStage());
-
-var_dump($pipeline->process(10));*/
-
-use Design\Repository\Storage\MemoryStorage;
-use Design\Repository\Storage\MysqlStorage;
-use Design\Repository\Storage\RedisStorage;
-use Design\Repository\PostRepository;
-use Design\Repository\Post;
+use Repository\Storage\MysqlStorage;
+use Repository\Storage\RedisStorage;
+use Repository\PostRepository;
+use Repository\Post;
 
 $db = new MysqlStorage();
 $redis = new RedisStorage();
